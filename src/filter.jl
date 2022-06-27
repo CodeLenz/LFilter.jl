75731d53802f725a01177977c26a33b2daa76ab5
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-27 18:21:45 -0400

    Initial commit

---
+++ src/filter.jl
@@ -0,0 +1,219 @@
+
+#
+# Main structure for filtering
+#
+mutable struct Filter
+
+   # Radius
+   radius::Float64
+
+   # Spatial
+   MAP::SparseMatrixCSC
+
+   # Positions with fixed elements
+   posfix::Array{Int64}
+
+   # Values of fixed elements
+   valfix::Array{Float64}
+
+   # Sharpness 
+   β::Float64
+   
+   # Center
+   η::Float64
+
+   # Densidade mínima
+   ρ_min::Float64 
+
+
+   function Filter(radius::T,mesh::Mesh,β::T=1.0,ρ_min=1E-3,η::T=0.5) where T
+
+      # If radius is 0.0, find a minimum radius 
+      if radius==zero(radius)
+         radius = max(mesh.bmesh.Lx/mesh.bmesh.nx, mesh.bmesh.Ly/mesh.bmesh.ny)*1.5
+      end
+
+      # Cria a matrix que mapeia as variáveis de projeto matemáticas
+      # para o vetor de densidades relativas (FILTRO)
+      # MAP é uma matriz esparsa
+      MAP = Map_x_ρ(radius,mesh)
+
+      # Force a 1<->1  mapping between ρ and x for each fixed element in the design
+      # and generate two lists: one containing the fixed elementos and ohter with
+      # the fixed values
+      posfix = Int64[]
+      valfix = T[]
+      if haskey(mesh.options,"fixed_element")
+
+         # Alias and number of fixed elements 
+         fixed_element =  mesh.options["fixed_element"]
+         nfixed = size(fixed_element,1)
+
+         for i=1:nfixed
+             ele = Int(fixed_element[i,1])
+             val = fixed_element[i,2]
+             MAP[ele,:] .= 0.0
+             MAP[ele,ele] = 1.0    
+             push!(posfix,ele)
+             push!(valfix,val)
+         end
+
+         # Remove zeros
+         dropzeros!(MAP)
+
+      end    
+    
+      new(radius,MAP,posfix,valfix,β,η,ρ_min)
+
+   end
+
+end
+
+
+
+#
+# Monta a matriz F que mapeia x para ρ, ou seja,
+# filtro de densidades padrão
+#
+function Map_x_ρ(R::T,mesh::Mesh) where T
+
+    # Alias
+    ne = mesh.bmesh.ne
+
+    # Obtem os vizinhos e pesos para o raio/malha
+    viz, weigths = Neighbours(R,mesh)
+
+    # Sparse matrix
+    VI = Int64[]
+    VJ = Int64[]
+    VV = T[]
+
+    # For each element, put the neighbours in columns. Lets
+    # divide by the sum of the weigths 
+    for ele=1:ne
+
+        # Sum of weigths
+        somat = sum(weigths[ele])
+
+        # Scan the neighbours
+        for par in zip(viz[ele],weigths[ele]/somat) 
+
+            push!(VI,ele)
+            push!(VJ,par[1])
+            push!(VV,par[2])
+             
+        end # zip
+    end # ele
+
+    return sparse(VI,VJ,VV)
+    
+end
+
+
+#
+# Operador para a projeção tanh. Este operador
+# não é linear (depende de ρ), portanto devemos 
+# fazer na forma algoritmica
+#
+function Map_ρ_tanh(β::T,η::T,ρ::Array{T,1}) where T
+
+    # Primeiro alocamos um vetor com a mesma dimensão 
+    # de ρ
+    ρ_proj = Array{T}(undef,length(ρ))
+
+    # Cte
+    cte = tanh(β*η)
+
+    # Loop sobre os elementos, calculando a projeção
+    for ele in LinearIndices(ρ)
+
+        ρ_proj[ele] = (tanh(β*(ρ[ele]-η))+cte)/(cte+tanh(β*(1-η)))
+
+    end
+
+    return ρ_proj
+
+end
+
+#
+# Operador de correção de derivada d ρ_proj / d ρ 
+# que será utilizado na regra da cadeia para correção de derivadas
+#
+function dρ_projdρ(β::T,η::T,ρ::Array{T,1}) where T
+
+    # Primeiro alocamos um vetor com a mesma dimensão 
+    # de ρ
+    operador = Array{T}(undef,length(ρ))
+
+    # Loop sobre os elementos, calculando a projeção
+    for ele in LinearIndices(ρ)
+
+         operador[ele] = (β*sech(β*(ρ[ele]-η))^2)/(tanh(β*η)+tanh(β*(1-η)))
+
+    end
+    
+    return Diagonal(operador)
+
+end
+
+
+#
+# do the Map 
+#
+function x2ρ(x::Array{T},filter::Filter) where T
+
+    # Assert fixed elements
+    x[filter.posfix] .= filter.valfix
+
+    # Map
+    ρ = filter.MAP*x
+     
+end
+
+
+#
+# do the proj -> this is the one to use 
+#
+function x2proj(x::Array{T},filter::Filter) where T
+
+    
+    # Mapeamento para as densidades - Filtro
+    ρ = x2ρ(x,filter)
+
+    # Projeção heaviside
+    ρ_proj = Map_ρ_tanh(filter.β,filter.η,ρ)
+
+    # Correção do rho mínimo
+    return filter.ρ_min .+ (1.0-filter.ρ_min).*ρ_proj
+
+
+end
+
+
+#
+# Fix the sensitivities
+#
+function dproj2dx!(x::Array{T},dproj::Array{T},filter::Filter) where T
+
+
+    # Mapeamento para as densidades - Filtro
+    ρ = x2ρ(x,filter)
+    
+    # Operador de correção da projeção (matriz diagonal)
+    R = dρ_projdρ(filter.β,filter.η,ρ) 
+
+    # Corrige as derivadas devido ao filtro
+    # Cuidado com o MAP'   
+    dx = R*filter.MAP'*dproj
+
+    # Corrige para o valor mínimo
+    dx .= dx.*(1.0-filter.ρ_min)
+
+    # Assert fixed elements
+    dx[filter.posfix] .= 0.0
+
+    dproj.= dx
+
+
+end
+
---
+++ src/viz.jl
@@ -0,0 +1,58 @@
+
+# Neighbours and weigths
+function Neighbours(mesh::Mesh,radius)
+
+    # Alias
+    ne = mesh.bmesh.be
+
+    # Maximum number of neighbours in the mesh
+    num_max = 0
+
+    # Tables to store the neighbours and weigts
+    viz = Vector{Int64}[]
+    weigths = Vector{Float64}[]
+
+     # Loop for each element
+     for ele=1:ne
+
+         # Pega a posição do centróide do elemento central
+         c_ele = Centroid(mesh,ele) 
+
+         # Local Arrays
+         viz_ele = Int64[]
+         weigths_ele = Float64[]
+
+         # Scan the mesh and store the neighbours of ele
+         for j=1:ne
+
+             # Centroid of j
+             c_j = Centroid(mesh,j) 
+
+             # Distance
+             d = norm(cj.-c_ele)
+
+             # If inside the radius...
+             if d<=R
+                 push!(viz_ele,j)
+                 push!(weigths_ele, 1.0 - d/R)
+             end #if
+
+         end #j
+
+        # Assert if the element has neighbours
+        length(viz_ele)>0 || throw("Neighbours:: element $ele does not have neighbours")
+
+        # Maximum number of neighbours until now
+        num_max = max(num_max,length(viz_ele))
+
+        # Store the neighbours for ele
+        push!(viz,viz_ele)
+        push!(weigths,weigths_ele)
+
+     end #ele
+
+     # Return 
+     return viz, weigths
+
+end
+
