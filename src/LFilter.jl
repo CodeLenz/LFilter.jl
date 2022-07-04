c02bf26822388a2d3aa9f352b830eb23884d35a8
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-07-04 17:02:23 -0400

    Update LFilter.jl

--- src/LFilter.jl
+++ src/LFilter.jl
@@ -3,11 +3,21 @@ module LFilter
 using BMesh, LMesh
 using LinearAlgebra, SparseArrays
 
+if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
+       @eval Base.Experimental.@optlevel 3
+   end
+
 include("viz.jl")
 include("filter.jl")
 
 export Filter
 export x2proj
 export dproj2dx!
+
+# Precompile
+precompile(Neighbours,Mesh2D,Float64))
+precompile(Neighbours,Mesh3D,Float64))
+precompile(Map_x_ρ,(Float64,Mesh2D))
+precompile(Map_x_ρ,(Float64,Mesh3D))
 
 end # module LFilter
